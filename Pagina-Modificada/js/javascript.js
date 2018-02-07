9cbb0b19d2567758f6c3765f98d368b88782208c
nekker141 <sergio141.96@gmail.com>
2018-02-07 19:12:46 +0100

    1

--- Pagina-Modificada/js/javascript.js
+++ Pagina-Modificada/js/javascript.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
-  $(".navbar-nav li, .masamuscular").mouseenter(function(){
+  $(".navbar-nav li:nth-child(1), .masamuscular").mouseenter(function(){
       $(".masamuscular").show();
     }).mouseleave(function(){
-        $(".masamuscular").hide();
-      });
+      $(".masamuscular").hide();
+    });
+
+  $(".navbar-nav li:nth-child(2), .quemadoresdegrasa").mouseenter(function(){
+      $(".quemadoresdegrasa").show();
+    }).mouseleave(function(){
+      $(".quemadoresdegrasa").hide();
+    });
+
+  $(".navbar-nav li:nth-child(3), .energia").mouseenter(function(){
+      $(".energia").show();
+    }).mouseleave(function(){
+      $(".energia").hide();
+    });
+  $(".navbar-nav li:nth-child(4), .productosalimenticios").mouseenter(function(){
+      $(".productosalimenticios").show();
+    }).mouseleave(function(){
+      $(".productosalimenticios").hide();
+    });
+
+  $(".lupa").click(function(){
+    $(".form-control").focus();
+});
 });
